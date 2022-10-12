d0af4538fd012912301ecfdd2d9901acaa51b04b
舒晓满 <651487447@qq.com>
2022-10-13 01:21:14 +0800

    feat: 修改时间

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,16 @@ const Index = () => {
           <Card title={<Row justify="center">新娘时间线（李琳）</Row>}>
             <Steps progressDot current={999} direction="vertical">
               <Step
-                title="6:00"
+                title="4:30"
                 subTitle={<Typography.Text strong>起床、洗漱</Typography.Text>}
               />
               <Step
-                title="6:10"
+                title="4:40"
                 subTitle={<Typography.Text strong>新娘化妆</Typography.Text>}
                 description="伴娘自行化妆、穿伴娘服"
               />
               <Step
-                title="7:10"
+                title="5:40"
                 subTitle={
                   <Typography.Text strong>
                     伴娘整理婚房，布置接亲游戏道具，锁门准备接亲
@@ -33,29 +33,29 @@ const Index = () => {
                 }
               />
               <Step
-                title="8:00"
+                title="6:30"
                 subTitle={<Typography.Text strong>吃早饭</Typography.Text>}
                 description="帮忙堵门的亲友到达新娘家"
               />
               <Step
-                title="8:10"
+                title="6:40"
                 subTitle={
                   <Typography.Text strong>新娘穿秀禾服</Typography.Text>
                 }
               />
               <Step
-                title="8:15"
+                title="6:45"
                 subTitle={
                   <Typography.Text strong>新娘与父母拍摄花絮</Typography.Text>
                 }
               />
               <Step
-                title="8:30"
+                title="7:00"
                 subTitle={<Typography.Text strong>堵门做游戏</Typography.Text>}
                 // description="听见鞭炮声，代表新郎来了"
               />
               <Step
-                title="9:00"
+                title="7:30"
                 subTitle={
                   <Typography.Text strong type="danger">
                     给新娘父母敬茶
@@ -70,28 +70,28 @@ const Index = () => {
           <Card title={<Row justify="center">新郎时间线（舒晓满）</Row>}>
             <Steps progressDot current={999} direction="vertical">
               <Step
-                title="6:30"
+                title="5:00"
                 subTitle={<Typography.Text strong>起床、洗漱</Typography.Text>}
               />
 
               <Step
-                title="7:15"
+                title="5:45"
                 subTitle={
                   <Typography.Text strong>结亲团车队集合</Typography.Text>
                 }
                 description="新郎家门口的横路"
               />
               <Step
-                title="7:30"
+                title="6:00"
                 subTitle={<Typography.Text strong>吃早饭</Typography.Text>}
                 description="伴郎提前穿好伴郎服"
               />
               <Step
-                title="7:40"
+                title="6:20"
                 subTitle={<Typography.Text strong>拍摄花絮</Typography.Text>}
               />
               <Step
-                title="8:00"
+                title="6:30"
                 subTitle={
                   <Typography.Text strong>
                     排好车队，绑丝带、贴喜字
@@ -100,18 +100,18 @@ const Index = () => {
                 description="车队数量6+1"
               />
               <Step
-                title="8:10"
+                title="6:50"
                 subTitle={<Typography.Text strong>出发接亲</Typography.Text>}
               />
               <Step
-                title="8:30"
+                title="7:00"
                 subTitle={
                   <Typography.Text strong>到新娘家门口</Typography.Text>
                 }
                 description="主婚车停到铺子门口"
               />
               <Step
-                title="9:00"
+                title="7:30"
                 subTitle={
                   <Typography.Text strong type="danger">
                     给新娘父母敬茶
@@ -120,24 +120,24 @@ const Index = () => {
                 description="父母准备改口红包"
               />
               <Step
-                title="9:10"
+                title="7:35"
                 subTitle={
                   <Typography.Text strong>店铺门口准备放礼花</Typography.Text>
                 }
                 description="伴郎团提前到店铺门口"
               />
               <Step
-                title="9:15"
+                title="7:40"
                 subTitle={<Typography.Text strong>去新郎家</Typography.Text>}
               />
               <Step
-                title="9:30"
+                title="8:00"
                 subTitle={
                   <Typography.Text strong>新郎家人放鞭炮迎接</Typography.Text>
                 }
               />
               <Step
-                title="9:50"
+                title="8:10"
                 subTitle={
                   <Typography.Text strong type="danger">
                     给新郎父母敬茶
@@ -146,16 +146,16 @@ const Index = () => {
                 description="父母准备改口红包"
               />
               <Step
-                title="10:00"
+                title="8:30"
                 subTitle={<Typography.Text strong>婚房拍照</Typography.Text>}
               />
               <Step
-                title="10:15"
+                title="9:30"
                 subTitle={<Typography.Text strong>出发去礼堂</Typography.Text>}
                 description="伴郎团提前到院子门口准备放礼花"
               />
               <Step
-                title="10:30"
+                title="10:00"
                 subTitle={
                   <Typography.Text strong>
                     新郎迎宾、新娘换衣服补妆
